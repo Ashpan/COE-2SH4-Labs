c812f48af2ee1080ac8f5128cb82ef93b22c3355
Ashpan Raskar <ashpanr@gmail.com>
2019-11-17 11:02:29 -0500

    finished lab 5
    much easier than i thought for those methods

--- Lab5/src/SLLSet.java
+++ Lab5/src/SLLSet.java
@@ -1,5 +1,3 @@
-import javax.net.ssl.SSLServerSocket;
-
 public class SLLSet {
     private int setSize;
     private SLLNode head;
@@ -7,61 +5,61 @@ public class SLLSet {
         this.head = new SLLNode(0, null);
         this.setSize = 0;
     }
-    public SLLSet(int[] sortedArray){
-        this();
+    public SLLSet(int[] sortedArray){ //runs first constructor to init the set
+        this(); //then adds values from the array to the set, iterating each node and element in array
         int i = 0;
         for (SLLNode curr = this.head; i<sortedArray.length; curr = curr.next) {
-            curr.value = sortedArray[i];
-            if(i+1<sortedArray.length)
-                curr.next = new SLLNode(0, null);
+            curr.next = new SLLNode(sortedArray[i], null);
             i++; this.setSize++;
         }
     }
-    public int getSize(){ return this.setSize; }
-    public SLLSet copy() {
-        int[] arr = new int[this.setSize];
+    public int getSize(){ return this.setSize; } //returns the setSize int
+    public SLLSet copy() { //iterates through each node in a set and copies this to a new set, copying each value to
+        SLLSet newSet = new SLLSet(); //another node
         SLLNode currentNode = this.head;
-        int i = 0;
-        for (SLLNode curr = this.head; i<arr.length; curr = curr.next) {
-            arr[i] = curr.value;
-            i++;
+        SLLNode newNode = newSet.head;
+        while(currentNode.next != null){
+            newNode.next = new SLLNode(currentNode.next.value, null);
+            currentNode = currentNode.next;
+            newNode = newNode.next;
+            newSet.setSize++;
         }
-        return new SLLSet(arr);
+        return newSet;
     }
-    public boolean isIn(int v){
-        if (head == null) {
+    public boolean isIn(int v){ //iterates through each node in a set checking if the value property is equal to v
+        if (this.head == null) { //if its empty or not equal at the end, it returns false, otherwise it returns true
             return false;
         }else{
-            for (SLLNode curr = this.head; curr != null; curr = curr.next) {
-                if (curr.value == v) {
+            for (SLLNode curr = this.head; curr.next != null; curr = curr.next) {
+                if (curr.next.value == v) {
                     return true;
                 }
-                this.setSize++;
             }
         }
         return false;
-    }
+    } //
 
-    public void add(int v){
+    public void add(int v){ //only runs if the element isnt in the set
         if(!(isIn(v))){
-            int i = 0;
-            for (SLLNode curr = this.head; i<this.setSize; curr = curr.next) {
-                if(v>curr.next.value){
-                    curr.next = new SLLNode(v, curr.next);
+            SLLNode curr;
+            for (curr = this.head; curr.next != null; curr = curr.next) {
+                if(v<curr.next.value){ //it check the next value of the node, if v is less than the next value
+                    curr.next = new SLLNode(v, curr.next); //add in v as the next of the current node and its next will
+                    this.setSize++; //be the old next of the current node. increment size by 1 and then end the function
+                    return;
                 }
-                i++; this.setSize++;
             }
+            curr.next = new SLLNode(v, null); //if it goes at the end, set the next to null instead
+            this.setSize++; // increment size.
         }
     }
 
     public void remove(int v){
-        if(!(this.isIn(v))){
-            int i = 0;
-            for (SLLNode curr = this.head; i<this.setSize; curr = curr.next) {
-                if(curr.next.value == v) {
-                    curr.next = curr.next.next;
-                }
-                i++; this.setSize++;
+        for (SLLNode curr = this.head; curr.next != null; curr = curr.next) {
+            if(curr.next.value == v) { //if the node value = v
+                curr.next = curr.next.next; //set the next value of the current node, to the 2nd next value
+                this.setSize--; //reduce size by 1
+                return; //end method
             }
         }
     }
@@ -71,135 +69,114 @@ public class SLLSet {
         SLLNode firstNode = this.head;
         SLLNode secondNode = s.head;
         SLLNode newNode = newSet.head;
-        while(firstNode.next != null && secondNode.next != null) {
-            if (firstNode.value == secondNode.value){
-                newNode.value = firstNode.value;
+        //run until either set reaches the last element
+        //if firstNode < secondNode -> add firstNode value to newNode and move newNode and firstNode to their next value
+        //if firstNode > secondNode -> add secondNode value to newNode and move newNode and secondNode to their next value
+        //if both are equal -> add either value to newNode and move every node to their next node
+
+        //after this iterate through whatever set we havent run through completely yet
+        //add all those elements to the rest of the newSet
+        while(firstNode.next != null && secondNode.next != null){
+
+            if(firstNode.next.value < secondNode.next.value){
+                newNode.next = new SLLNode(firstNode.next.value, null);
                 firstNode = firstNode.next;
+                newNode = newNode.next;
+                newSet.setSize++;
+            }else if(firstNode.next.value > secondNode.next.value){
+                newNode.next = new SLLNode(secondNode.next.value, null);
                 secondNode = secondNode.next;
-            }else if(firstNode.value < secondNode.value) {
-                newNode.value = firstNode.value;
-                firstNode = firstNode.next;
+                newNode = newNode.next;
+                newSet.setSize++;
             }else{
-                newNode.value = secondNode.value;
+                newNode.next = new SLLNode(firstNode.next.value, null);
+                firstNode = firstNode.next;
                 secondNode = secondNode.next;
+                newNode = newNode.next;
+                newSet.setSize++;
             }
-            newSet.setSize++;
-            newNode.next = new SLLNode(0, null);
-            newNode = newNode.next;
-
         }
-        int temp = secondNode.value;
         while(firstNode.next != null){
-            if(temp <= firstNode.value){
-                newNode.value = temp;
-                temp = Integer.MAX_VALUE;
-                newSet.setSize++;
-                newNode.next = new SLLNode(0, null);
-                newNode = newNode.next;
-                firstNode = firstNode.next;
-            }else if(firstNode.next != null){
-                newNode.value = firstNode.value;
-                newSet.setSize++;
-                newNode.next = new SLLNode(0, null);
-                newNode = newNode.next;
-                firstNode = firstNode.next;
-            }
-            if(firstNode.next == null){
-                newNode.value = firstNode.value;
-                newSet.setSize++;
-                if(temp != Integer.MAX_VALUE)
-                    newNode.next = new SLLNode(temp, null);
-                    newSet.setSize++;
-            }
+            newNode.next = new SLLNode(firstNode.next.value, null);
+            firstNode = firstNode.next;
+            newNode = newNode.next;
+            newSet.setSize++;
         }
-
-        temp = firstNode.value;
         while(secondNode.next != null){
-            if(temp <= secondNode.value){
-                newNode.value = temp;
-                temp = Integer.MAX_VALUE;
-                newSet.setSize++;
-                newNode.next = new SLLNode(0, null);
-                newNode = newNode.next;
-                secondNode = secondNode.next;
-            }else if(secondNode.next != null){
-                newNode.value = secondNode.value;
-                newSet.setSize++;
-                newNode.next = new SLLNode(0, null);
-                newNode = newNode.next;
-                secondNode = secondNode.next;
-            }
-            if(secondNode.next == null){
-                newNode.value = secondNode.value;
-                newSet.setSize++;
-            }
+            newNode.next = new SLLNode(secondNode.next.value, null);
+            secondNode = secondNode.next;
+            newNode = newNode.next;
+            newSet.setSize++;
         }
-        return newSet;
+        return newSet; //return newSet
     }
-    public SLLSet intersection(SLLSet s){
+    public SLLSet intersect(SLLSet s){
         SLLSet newSet = new SLLSet();
         SLLNode firstNode = this.head;
         SLLNode secondNode = s.head;
         SLLNode newNode = newSet.head;
-        while(firstNode.next != null && secondNode.next != null) {
-            if (firstNode.value == secondNode.value){
-                newNode.value = firstNode.value;
-                newSet.setSize++;
-                newNode.next = new SLLNode(0, null);
+
+        while(firstNode.next != null && secondNode.next != null){ //run while neither set is on last node
+
+            if(firstNode.next.value < secondNode.next.value){ //if firstnode < second
+                firstNode = firstNode.next; //change firstnode to its next value
+            }else if(firstNode.next.value > secondNode.next.value){ //if firstnode > second
+                secondNode = secondNode.next; //change secondnode to its next value
+            }else{
+                newNode.next = new SLLNode(firstNode.next.value, null); //if they're equal, add the value of
+                firstNode = firstNode.next; //either node to the newNode and create the next mode
+                secondNode = secondNode.next; //move all the nodes to the next node
                 newNode = newNode.next;
-                firstNode = firstNode.next;
-                secondNode = secondNode.next;
-            }else if(firstNode.value < secondNode.value)
-                firstNode = firstNode.next;
-            else
-                secondNode = secondNode.next;
+                newSet.setSize++;
+            }
         }
-        return newSet;
+        return newSet; //return the new set
     }
 
     public SLLSet difference(SLLSet s){
         SLLSet newSet = new SLLSet();
-        SLLNode current = this.head;
-        SLLNode newCurrent = newSet.head;
-        for (int i = 0; i < this.setSize; i++) {
-            if(!(s.isIn(current.value))){
-                System.out.println("curr " + current.value);
-                newCurrent.value = current.value;
-                if(current.next != null){
-                    newCurrent.next = new SLLNode(0, null);
-                    newCurrent = newCurrent.next;
-                }
+        SLLNode firstNode = this.head;
+        SLLNode newNode = newSet.head;
+        while(firstNode.next != null){ //run until you reach last node of firstSet
+            if(!s.isIn(firstNode.next.value)){ //if the element of this set is not in the s set
+                newNode.next = new SLLNode(firstNode.next.value, null); //then add it to the newSet node
+                newNode = newNode.next; //move new node to next value
                 newSet.setSize++;
             }
-            current = current.next;
+            firstNode = firstNode.next; //move this node to next value
         }
-        return newSet;
+        return newSet; //return the new set
     }
-
     public static SLLSet union( SLLSet[] sArray){
-        SLLSet unionSet = sArray[0].union(new SLLSet(new int[]{}));
-        for (int i = 0; i < sArray.length-1; i++) {
-            unionSet = unionSet.union(sArray[i+1]);
+        SLLSet unionSet = new SLLSet(); //create a empty union set
+        for (int i = 0; i < sArray.length; i++) {
+            unionSet = unionSet.union(sArray[i]); //set it to the union of itself and the ith set in the array
         }
-        return unionSet;
+        return unionSet; //return that new set
     }
 
     public String toString(){
-        String str = "";
+        String str = ""; //initialize string
         SLLNode curr = this.head;
         for (int i = 0; i < this.setSize; i++) {
-            str += curr.value + ", ";
-            curr = curr.next;
+            if(curr.next.next != null) //if the next value is the last value, dont run this next part
+                str += curr.next.value + ", "; //concatenate the value plus a comma to str
+            else{
+                str += curr.next.value; //concatenate just the value to str
+            }
+            curr = curr.next; //iterate the node
         }
-        str = str.substring(0,str.length()-2);
-        return str;
+        return str; //return str
     }
     public static void main(String[] args) {
         SLLSet set1 = new SLLSet(new int[]{1, 2, 4, 11, 13, 14, 16});
-        SLLSet set2 = new SLLSet(new int[]{5, 6});
+        SLLSet set2 = new SLLSet(new int[]{4, 5, 9});
+        SLLSet[] setArr = new SLLSet[]{new SLLSet(new int[] {1,2,3}), new SLLSet(new int[] {1,2,7}), new SLLSet(new int[] {3,5,6})};
         System.out.println(set1.toString());
         System.out.println(set2.toString());
-        System.out.println(set1.intersection(set2).toString());
+        System.out.println(set1.difference(set2).toString());
+        System.out.println(union(setArr).toString());
+        set2.remove(9);
+        System.out.println(set2.toString());
     }
-//    TODO: Check if the empty set works!
 }
